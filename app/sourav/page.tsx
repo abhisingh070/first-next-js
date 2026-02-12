004e7810317065a92ef731963bb623de62d447a6
abhisingh070 <abhisingh790333@gmail.com>
2026-02-13 00:02:49 +0530

    this is my first next js application

---
+++ app/sourav/page.tsx
@@ -0,0 +1,6 @@
+export default function Sourav() {
+    return <div className="bg-blue-500 p-4 align-items items-center justify-center flex h-screen flex-col">
+        <h1 className="text-white text-2xl">sourav this side</h1>
+        <p>let me know who am i </p>
+    </div>
+}
